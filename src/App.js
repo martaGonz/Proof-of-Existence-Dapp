3ce52e0d973e3990856d2a6e05607701ddda9126
Maruta <marta82@gmail.com>
2018-08-28 16:13:57 +0900

    dragon included

--- src/App.js
+++ src/App.js
@@ -134,7 +134,8 @@ class App extends Component {
           <div className="pure-g">
             <div className="pure-u-1-1">
               <h1>Your Image</h1>
-              <p>This DApp saves your images on the Ethereum Blockchain</p>
+              <p>Proof of Existence Dragon DApp saves your images on the Ethereum Blockchain</p>
+              <img src={`https://github.com/martagonz/proof-of-existence-dapp/public/dragon.png`} alt=""/>
               <h2>Upload Image</h2>
               <form onSubmit={this.onSubmit} >
                 <input type='file' onChange={this.captureFile} />
